ba8f5e6792292ecc33f49f9d0eef3e3d2c0b539f
Firelonze <26387@ma-web.nl>
2019-05-14 15:07:02 +0200

    Mini Progress Tracking

--- Robin WIC 2.0/Assets/Scripts/Patrol.cs
+++ Robin WIC 2.0/Assets/Scripts/Patrol.cs
@@ -1,27 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.AI;
 public class Patrol : MonoBehaviour
 {
-    public float speed;
-    public float distance;
-    public Transform groundDetection;
-    private bool movingRight = true;
-    
-    void Update()
+    private void Start()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
-
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance);
-        if(groundInfo.collider == false) {
-            transform.eulerAngles = new Vector3(0, -180, 0);
-            movingRight = false;
-        }
-        else
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-            movingRight = true;
-        }
+        
+    }
+    private void Update()
+    {
+        
     }
 }
