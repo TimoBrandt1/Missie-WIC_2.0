adcded71a985a007151dff2018419b7571a46cad
TimoBrandt1 <timobrandt2002@gmail.com>
2019-05-15 10:25:59 +0200

    Started With Enemy Scripts

---
+++ Missie WIC 2.0/Assets/Scripts/EnemyFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class EnemyFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public GameObject player;
+    public float speed;
+    private Vector3 startPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
