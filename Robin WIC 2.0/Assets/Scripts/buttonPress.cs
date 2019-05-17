d6c60fb8cc1bde0064364f31265f9829b882a441
Firelonze <26387@ma-web.nl>
2019-05-17 10:27:02 +0200

    17.5,2019

--- Robin WIC 2.0/Assets/Scripts/buttonPress.cs
+++ Robin WIC 2.0/Assets/Scripts/buttonPress.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class buttonPress : MonoBehaviour
 {
     public GameObject player;
+    public GameObject nasa;
+    public GameObject nasa2;
+    public GameObject nasa3;
+    public GameObject nasa4;
     private bool ReversedGravity = false;
     // Start is called before the first frame update
     void Start()
@@ -23,13 +27,31 @@ public class buttonPress : MonoBehaviour
             if (ReversedGravity == false)
             {
                 player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                player.gameObject.transform.eulerAngles = new Vector3(0, 0, 180);
+                nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                player.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+                nasa.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+                nasa2.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+                nasa3.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+                nasa4.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+
                 ReversedGravity = true;
             }
             else
             {
                 player.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+                nasa.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+                nasa2.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+                nasa3.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+                nasa4.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
                 player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= 1;
+                nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= 1;
+                nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= 1;
+                nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= 1;
+                nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= 1;
+
                 ReversedGravity = false;
             }
         }
