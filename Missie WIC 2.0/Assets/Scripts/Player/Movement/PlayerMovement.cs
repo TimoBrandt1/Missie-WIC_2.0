815af47831d38a0f2f4ff1f7b209a89cd4200a18
TimoBrandt1 <timobrandt2002@gmail.com>
2019-05-19 14:35:37 +0200

    Started With Player Animations

--- Missie WIC 2.0/Assets/Scripts/Player/Movement/PlayerMovement.cs
+++ Missie WIC 2.0/Assets/Scripts/Player/Movement/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
 
     public float runSpeed = 40f;
 
@@ -17,9 +18,12 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if (Input.GetButtonDown("Crouch"))
         {
@@ -29,6 +33,17 @@ public class PlayerMovement : MonoBehaviour
         {
             crouch = false;
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching)
+    {
+        animator.SetBool("IsCrouching", isCrouching);
+
     }
 
     void FixedUpdate()
