3df0ae1bda31f954a1cec0cea848d294dd88cdfa
Firelonze <26387@ma-web.nl>
2019-05-22 01:13:23 +0200

    Gui ON MAIN Allert MIGHt NOT work

---
+++ Missie WIC 2.0/Assets/Scripts/UI/PickUpScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpScript : MonoBehaviour
+{
+    private Inventory inventory;
+    public GameObject GunButton;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if (inventory.isFull[i] == false)
+                {
+                    //ITEM CAN BE ADDED TO INVENTORY !
+                    inventory.isFull[i] = true;
+                    Instantiate(GunButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+
+            }
+        }
+    }
+}
+
