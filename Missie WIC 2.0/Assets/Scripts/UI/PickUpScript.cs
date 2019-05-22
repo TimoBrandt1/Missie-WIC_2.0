56b7dfc5857917d02027eddaa6d408e1f1249c44
Firelonze <26387@ma-web.nl>
2019-05-22 09:39:15 +0200

    Main UI Inventory Shitstick

--- Missie WIC 2.0/Assets/Scripts/UI/PickUpScript.cs
+++ Missie WIC 2.0/Assets/Scripts/UI/PickUpScript.cs
@@ -11,17 +11,19 @@ public class PickUpScript : MonoBehaviour
     {
         inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             for (int i = 0; i < inventory.slots.Length; i++)
             {
-                if (inventory.isFull[i] == false)
+                if (!inventory.isFull[i])
                 {
                     //ITEM CAN BE ADDED TO INVENTORY !
                     inventory.isFull[i] = true;
-                    Instantiate(GunButton, inventory.slots[i].transform, false);
+                    GameObject currentItemInSlot = Instantiate(GunButton, inventory.slots[i].transform, false);
+                    currentItemInSlot.transform.position = inventory.slots[i].transform.position;
                     Destroy(gameObject);
                     break;
                 }
