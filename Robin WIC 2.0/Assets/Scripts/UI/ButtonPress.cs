06fd00784a36938a7f2d12a365e97a3ed61f3b73
Firelonze <26387@ma-web.nl>
2019-05-23 21:31:42 +0200

    Bruh shit dat niet nodig is

--- Robin WIC 2.0/Assets/Scripts/UI/ButtonPress.cs
+++ Robin WIC 2.0/Assets/Scripts/UI/ButtonPress.cs
@@ -8,6 +8,35 @@ public class ButtonPress : CharacterController2D
     public GameObject nasa3;
     public GameObject nasa4;
     public bool ReversedGravity = false;
+    private bool FaceRight = true;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            if (ReversedGravity == true && FaceRight == false)
+            {
+                player.gameObject.transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+            if (ReversedGravity == true && FaceRight == true)
+            {
+                player.gameObject.transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            FaceRight = false;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (ReversedGravity == false && FaceRight == true)
+            {
+                player.gameObject.transform.eulerAngles = new Vector3(180, 0, 0);
+            }
+            if (ReversedGravity == false && FaceRight == false)
+            {
+                player.gameObject.transform.eulerAngles = new Vector3(180, 180, 0);
+            }
+            FaceRight = true;
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Button")
