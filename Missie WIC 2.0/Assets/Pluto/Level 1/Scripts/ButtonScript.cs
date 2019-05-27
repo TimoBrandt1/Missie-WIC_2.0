a40e86210322d3e32448fa88604b971794aa0584
Firelonze <26387@ma-web.nl>
2019-05-27 12:38:05 +0200

    Main Reversed

---
+++ Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/ButtonScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ButtonScript : CharacterController2D
+{
+    public GameObject player;
+    public GameObject nasa;
+    public GameObject nasa2;
+    public GameObject nasa3;
+    public GameObject nasa4;
+    public int VectorX;
+    private void Start()
+    {
+        VectorX = 180;
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Button")
+        {
+                player.gameObject.transform.eulerAngles = new Vector3(0 - VectorX,0,0);
+                nasa.gameObject.transform.eulerAngles *= -1;
+                nasa2.gameObject.transform.eulerAngles *= -1;
+                nasa3.gameObject.transform.eulerAngles *= -1;
+                nasa4.gameObject.transform.eulerAngles *= -1;
+                player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+                player.GetComponent<CharacterController2D>().m_JumpForce *= -1;
+         }
+    }
+}
