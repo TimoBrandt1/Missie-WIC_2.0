0a39f252603ce1c7fd8459475e6370a22b764a95
Firelonze <26387@ma-web.nl>
2019-05-29 10:28:28 +0200

    Bur

--- Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/ButtonScript.cs
+++ Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/ButtonScript.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class ButtonScript : CharacterController2D
 {
@@ -7,26 +8,36 @@ public class ButtonScript : CharacterController2D
     public GameObject nasa2;
     public GameObject nasa3;
     public GameObject nasa4;
-    public int VectorX;
-    private void Start()
-    {
-        VectorX = 180;
-    }
+    public GameObject[] enemies;
+
+    private int posX = 0;
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Button")
+        if (collision.gameObject.tag == "Player")
         {
-                player.gameObject.transform.eulerAngles = new Vector3(0 - VectorX,0,0);
-                nasa.gameObject.transform.eulerAngles *= -1;
-                nasa2.gameObject.transform.eulerAngles *= -1;
-                nasa3.gameObject.transform.eulerAngles *= -1;
-                nasa4.gameObject.transform.eulerAngles *= -1;
-                player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-                player.GetComponent<CharacterController2D>().m_JumpForce *= -1;
+            posX -= 180;
+            player.gameObject.transform.eulerAngles = new Vector3(posX,0,0);
+            /*nasa.gameObject.transform.eulerAngles = new Vector3(posX, 0, 0);
+            nasa2.gameObject.transform.eulerAngles = new Vector3(posX, 0, 0);
+            nasa3.gameObject.transform.eulerAngles = new Vector3(posX, 0, 0);
+            nasa4.gameObject.transform.eulerAngles = new Vector3(posX, 0, 0);
+            */
+            player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            /*
+            nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            */
+            player.GetComponent<CharacterController2D>().m_JumpForce *= -1;
+
+            foreach(GameObject enem in enemies)
+            {
+                enem.transform.eulerAngles = new Vector3(posX, 0, 0);
+                enem.GetComponent<Rigidbody2D>().gravityScale *= -1;
+            }
+
+
          }
     }
 }
--- Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/Enemy/Patrol.cs
+++ Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/Enemy/Patrol.cs
@@ -9,20 +9,26 @@ public class Patrol : MonoBehaviour
     public float speed;
     private bool moveingRight;
     private NavMeshAgent agent;
+    Vector2 direction;
     private void Start()
     {
         moveingRight = true;
+        //direction = Vector2.right;
         agent = gameObject.GetComponent<NavMeshAgent>();
     }
-    private void Update()
+    void Update()
     {
+        Debug.Log("LOl");
         if (moveingRight == true)
         {
+            //transform.Translate(direction* speed * Time.deltaTime);
             transform.Translate(Vector2.right * speed * Time.deltaTime);
         }
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        direction = -direction;
         if (other.gameObject.tag == "1")
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
