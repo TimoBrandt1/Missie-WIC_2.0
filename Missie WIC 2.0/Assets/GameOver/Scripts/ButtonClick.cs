582c85c58384e2e453ee515ebb4ebcfa1be7bd30
TimoBrandt1 <timobrandt2002@gmail.com>
2019-06-02 17:38:23 +0200

    Almost done with buttons
    Still Need To fix scaling isueses

---
+++ Missie WIC 2.0/Assets/GameOver/Scripts/ButtonClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonClick : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Mainscreen");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+}
---
+++ Missie WIC 2.0/Assets/GameOver/Scripts/ButtonSpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonSpawn : MonoBehaviour
+{
+    //MMB Stands for MainMenuButton
+    public GameObject MMB_Prefab;
+    private GameObject MMB;
+
+    //RB Stands for RestartButton
+    public GameObject RB_Prefab;
+    private GameObject RB;
+
+    public GameObject PanelButtonParent;
+
+    public Transform MMB_SpawnPos;
+    public Transform RB_SpawnPos;
+
+    void Start()
+    {
+        MMB = Instantiate(MMB_Prefab, MMB_SpawnPos.position, MMB_SpawnPos.rotation);
+        RB = Instantiate(RB_Prefab, RB_SpawnPos.position, RB_SpawnPos.rotation);
+        StartCoroutine(WaitForIntro());
+    }
+
+    IEnumerator WaitForIntro()
+    {
+        yield return new WaitForSeconds(2);
+        MMB.transform.SetParent(PanelButtonParent.transform);
+        RB.transform.SetParent(PanelButtonParent.transform);
+    }
+}
