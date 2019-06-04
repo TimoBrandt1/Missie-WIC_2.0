2bc41c1d3373caa64bc58ac9157ac642653a6619
Firelonze <26387@ma-web.nl>
2019-06-04 14:39:13 +0200

    Button Completed

--- Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/ButtonPress.cs
+++ Missie WIC 2.0/Assets/Pluto/Level 1/Scripts/ButtonPress.cs
@@ -13,7 +13,6 @@ public class ButtonPress : MonoBehaviour
     public bool Test = true;
     private void Update()
     {
-        Debug.Log(posX);
         /* if (Input.GetKeyDown(KeyCode.A))
          {
              facingRight = false;
@@ -38,25 +37,8 @@ public class ButtonPress : MonoBehaviour
     }
         
     private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Player" && Test == true && posX == 180)
-        {
-            posX = 0;
-            player.transform.Rotate(posX, 0, 0);
-            nasa.transform.Rotate(posX, 0, 0);
-            nasa2.transform.Rotate(posX, 0, 0);
-            nasa3.transform.Rotate(posX, 0, 0);
-            nasa4.transform.Rotate(posX, 0, 0);
-            player.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            nasa.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            nasa2.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            nasa3.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            nasa4.gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
-            player.GetComponent<CharacterController2D>().m_JumpForce *= -1;
-
-            Test = false;
-        }
-        if (collision.gameObject.tag == "Player" && Test == true && posX == 0)
+    { 
+        if (collision.gameObject.tag == "Player" && Test == true)
         {
             posX = 180;
             player.transform.Rotate(posX, 0, 0);
