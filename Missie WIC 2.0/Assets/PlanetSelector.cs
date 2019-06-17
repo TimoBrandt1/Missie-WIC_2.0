4527e8d58fb5f9b7d1e92f86a63025dd12f2bdb3
Firelonze <26387@ma-web.nl>
2019-06-17 15:24:17 +0200

    Animation select screen code missing

---
+++ Missie WIC 2.0/Assets/PlanetSelector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelector : MonoBehaviour
+{
+    private ArrayList[] planet;
+    public bool PlutoSelected;
+    private void Start()
+    {
+        planet = Pluto;
+    }
+    void Update()
+    {
+        if (planet == Pluto)
+        {
+            PlutoSelected = true;
+        }
+    }
+}
