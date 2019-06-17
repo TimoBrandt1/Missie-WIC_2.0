4849b8c2d043312496293df2015a35be31aab985
Firelonze <26387@ma-web.nl>
2019-06-17 16:04:03 +0200

    Script made

--- Missie WIC 2.0/Assets/PlanetSelector.cs
+++ Missie WIC 2.0/Assets/PlanetSelector.cs
@@ -4,17 +4,95 @@ using UnityEngine;
 
 public class PlanetSelector : MonoBehaviour
 {
-    private ArrayList[] planet;
+    private string planets;
+    private int planetCounter = 0;
     public bool PlutoSelected;
-    private void Start()
-    {
-        planet = Pluto;
-    }
+    public bool NeptuneSelected;
+    public bool UranusSelected;
+    public bool SaturnSelected;
+    public bool JupiterSelected;
+    public bool MarsSelected;
+    public bool MoonSelected;
+    public bool EarthSelected;
+    public bool VenusSelected;
+    public bool MercurySelected;
     void Update()
     {
-        if (planet == Pluto)
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            planetCounter++;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            planetCounter++;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            planetCounter--;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            planetCounter--;
+        }
+        CheckCounter();
+        CheckPlanet();
+    }
+    void CheckPlanet()
+    {
+        if (planets == "Pluto")
         {
             PlutoSelected = true;
         }
+        if (planets == "Neptune")
+        {
+            NeptuneSelected = true;
+        }
+        if (planets == "Uranus")
+        {
+            UranusSelected = true;
+        }
+        if (planets == "Saturn")
+        {
+            SaturnSelected = true;
+        }
+        if (planets == "Jupiter")
+        {
+            JupiterSelected = true;
+        }
+        if (planets == "Mars")
+        {
+            MarsSelected = true;
+        }
+        if (planets == "Earth")
+        {
+            EarthSelected = true;
+        }
+        if (planets == "Venus")
+        {
+            VenusSelected = true;
+        }
+        if (planets == "Mercury")
+        {
+            MercurySelected = true;
+        }
+    }
+    void CheckCounter()
+    {
+        if (planetCounter == 1)
+        {
+            planets = "Pluto";
+        }
+        if (planetCounter == 2)
+        {
+            planets = "Neptune";
+        }
+        //Uranus
+        //Saturnus
+        //Jupiter
+        //Mars
+        //Maan
+        //Aarde
+        //Venus
+        //Mercurius
     }
 }
