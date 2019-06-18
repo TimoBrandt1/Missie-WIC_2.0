ff261bdc484f5b76f4b3584d0c9d337969504078
Firelonze <26387@ma-web.nl>
2019-06-18 15:11:04 +0200

    LevelSelectie

---
+++ Missie WIC 2.0/Assets/LevelSelectionProgress.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelectionProgress : MonoBehaviour
+{
+    public Animator animator;
+    public int stage;
+    private void Update()
+    {
+        stage = PlayerPrefs.GetInt("stage");
+        Debug.Log(stage);
+        if (stage == 1)
+        {
+            animator.SetBool("SecondLevelUnlocked", true);
+        }
+        if (stage == 2)
+        {
+            animator.SetBool("ThirdLevelUnlocked", true);
+        }
+        if (stage == 3)
+        {
+            animator.SetBool("FourthLevelUnlocked", true);
+        }
+        if (stage == 4)
+        {
+            animator.SetBool("FifthLevelUnlocked", true);
+        }
+        if (stage == 5)
+        {
+            animator.SetBool("AllLevelsUnlocked", true);
+        }
+    }
+}
--- Missie WIC 2.0/Assets/PlanetSelector.cs
+++ Missie WIC 2.0/Assets/PlanetSelector.cs
@@ -6,20 +6,10 @@ public class PlanetSelector : MonoBehaviour
 {
     public Animator animator;
     private string planets;
-    private int planetCounter = 0;
+    private int planetCounter;
     public bool PlutoSelected;
-    public bool NeptuneSelected;
-    public bool UranusSelected;
-    public bool SaturnSelected;
-    public bool JupiterSelected;
-    public bool MarsSelected;
-    public bool MoonSelected;
-    public bool EarthSelected;
-    public bool VenusSelected;
-    public bool MercurySelected;
     void Update()
     {
-        Debug.Log(planetCounter);
         if (Input.GetKey(KeyCode.RightArrow))
         {
             planetCounter =+ 1;
@@ -50,80 +40,12 @@ public class PlanetSelector : MonoBehaviour
             animator.SetBool("PlutoSelected", true);
             PlutoSelected = true;
         }
-        if (planets == "Neptune")
-        {
-            NeptuneSelected = true;
-        }
-        if (planets == "Uranus")
-        {
-            UranusSelected = true;
-        }
-        if (planets == "Saturn")
-        {
-            SaturnSelected = true;
-        }
-        if (planets == "Jupiter")
-        {
-            JupiterSelected = true;
-        }
-        if (planets == "Mars")
-        {
-            MarsSelected = true;
-        }
-        if (planets == "Earth")
-        {
-            EarthSelected = true;
-        }
-        if (planets == "Venus")
-        {
-            VenusSelected = true;
-        }
-        if (planets == "Mercury")
-        {
-            MercurySelected = true;
-        }
     }
     void CheckCounter()
     {
         if (planetCounter == 1)
         {
             planets = "Pluto";
-        }
-        if (planetCounter == 2)
-        {
-            planets = "Neptune";
-        }
-        if (planetCounter == 3)
-        {
-            planets = "Uranus";
-        }
-        if (planetCounter == 4)
-        {
-            planets = "Saturn";
-        }
-        if (planetCounter == 5)
-        {
-            planets = "Jupiter";
-        }
-        if (planetCounter == 6)
-        {
-            planets = "Mars";
-        }
-        if (planetCounter == 7)
-        {
-            planets = "Moon";
-        }
-        if (planetCounter == 8)
-        {
-            planets = "Earth";
-        }
-        if (planetCounter == 9)
-        {
-            planets = "Venus";
-        }
-        if (planetCounter == 10)
-        {
-            planets = "Mercury";
         }
     }
 }
