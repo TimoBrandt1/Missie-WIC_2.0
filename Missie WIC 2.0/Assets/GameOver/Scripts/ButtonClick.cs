bf058d76167f2c2e10d08c0e694dd13d79ec1ba5
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-06-21 09:18:05 +0200

    Gameover Restart Compleate

--- Missie WIC 2.0/Assets/GameOver/Scripts/ButtonClick.cs
+++ Missie WIC 2.0/Assets/GameOver/Scripts/ButtonClick.cs
@@ -5,12 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class ButtonClick : MonoBehaviour
 {
+    public int CurrentLevel;
+    void Update()
+    {
+        CurrentLevel = PlayerPrefs.GetInt("CurrentLevel");
+    }
     public void MainMenu()
     {
         SceneManager.LoadScene("Mainscreen");
     }
     public void Restart()
     {
-        SceneManager.LoadScene("Tutorial");
+        if (CurrentLevel == 0)
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        if (CurrentLevel == 1)
+        {
+            SceneManager.LoadScene("level 1");
+        }
+        if (CurrentLevel == 2)
+        {
+            SceneManager.LoadScene("level 2");
+        }
+        if (CurrentLevel == 3)
+        {
+            SceneManager.LoadScene("level 3");
+        }
+        if (CurrentLevel == 4)
+        {
+            SceneManager.LoadScene("level 4");
+        }
+        if (CurrentLevel == 5)
+        {
+            SceneManager.LoadScene("level 5");
+        }
     }
 }
--- Missie WIC 2.0/Assets/PauseMenu/PauseMenu.cs
+++ Missie WIC 2.0/Assets/PauseMenu/PauseMenu.cs
@@ -94,7 +94,7 @@ public class PauseMenu : MonoBehaviour
     public void LoadMenu()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Mainscreen");
+        SceneManager.LoadScene("PlanetSelection");
     }
 
     public void QuitGame()
