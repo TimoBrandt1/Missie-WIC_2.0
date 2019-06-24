abd054acb08a15260d4378c882006475b34b51f1
Firelonze <26387@ma-web.nl>
2019-06-24 11:10:49 +0200

    Platforms naar voren gehaald

--- Missie WIC 2.0/Assets/LevelSelectionProgress.cs
+++ Missie WIC 2.0/Assets/LevelSelectionProgress.cs
@@ -6,28 +6,28 @@ using UnityEngine.SceneManagement;
 public class LevelSelectionProgress : MonoBehaviour
 {
     public Animator animator;
-    public int stage;
+    public int stage = 0;
     private void Update()
     {
         stage = PlayerPrefs.GetInt("stage");
         Debug.Log(stage);
-        if (stage == 1)
+        if (stage == 2)
         {
             animator.SetInteger("Level", 1);
         }
-        if (stage == 2)
+        if (stage == 3)
         {
             animator.SetInteger("Level", 2);
         }
-        if (stage == 3)
+        if (stage == 4)
         {
             animator.SetInteger("Level", 3);
         }
-        if (stage == 4)
+        if (stage == 5)
         {
             animator.SetInteger("Level", 4);
         }
-        if (stage == 5)
+        if (stage == 6)
         {
             animator.SetInteger("Level", 5);
         }
