366c38a4e271d18438be555195fc16e29c7c4b9c
Firelonze <26387@ma-web.nl>
2019-06-25 14:07:46 +0200

    Strip Added & scene switch changed

---
+++ Missie WIC 2.0/Assets/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    bool videoEnd = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForVideoEnd());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator WaitForVideoEnd()
+    {
+        yield return new WaitForSeconds(14);
+        SceneManager.LoadScene("PlutoLevelSelectie");
+    }
+}
