0706f47fb28aa36f40045bedcee563241187e920
TimoBrandt1 <41124729+TimoBrandt1@users.noreply.github.com>
2019-06-25 12:19:53 +0200

    update

---
+++ Missie WIC 2.0/Assets/TitleScreen/Scripts/ToMainScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToMainScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Mainscreen");
+    }
+}
